e408278d5008f7f42ecf6f2f21abe1b6d07a7623
Alexander Færøy <ahf@0x90.dk>
2015-02-22 14:29:44 +0100

    Move the high-level API to enacl_ext

--- src/enacl.erl
+++ src/enacl.erl
@@ -60,18 +60,16 @@
 	onetime_auth_verify/3
 ]).
 
+%% Curve 25519.
+-export([
+	curve25519_scalarmult/2
+]).
+
 %% Low-level functions
 -export([
 	hash/1,
 	verify_16/2,
 	verify_32/2
-]).
-
-%% Curve25519
--export([
-	curve25519_keypair/0,
-	curve25519_public_key/1,
-	curve25519_shared/2
 ]).
 
 %% Libsodium specific functions (which are also part of the "undocumented" interface to NaCl
@@ -174,32 +172,6 @@ verify_16(_, _) -> error(badarg).
 -spec verify_32(binary(), binary()) -> boolean().
 verify_32(X, Y) when is_binary(X), is_binary(Y) -> enacl_nif:crypto_verify_32(X, Y);
 verify_32(_, _) -> error(badarg).
-
-%% Curve 25519 Crypto
-%% ------------------
-%% @doc curve25519_keypair/0 creates a new Public/Secret keypair.
-%%
-%% Generates and returns a new key pair for the Curve 25519 encryption scheme. The return value is a
-%% map in order to avoid using the public key as a secret key and vice versa.
-%% @end.
--spec curve25519_keypair() -> #{ atom() => binary() }.
-curve25519_keypair() ->
-	<<B0:8/integer, B1:30/binary, B2:8/integer>> = randombytes(32),
-	SK = <<(B0 band 248), B1/binary, (64 bor (B2 band 127))>>,
-	PK = curve25519_public_key(SK),
-	#{ public => PK, secret => SK }.
-
-%% @doc curve25519_public_key/1 creates a public key from a given SecretKey.
-%% @end
--spec curve25519_public_key(SecretKey :: binary()) -> binary().
-curve25519_public_key(SecretKey) ->
-	enacl_nif:crypto_curve25519_scalarmult(SecretKey, <<9, 0:248>>).
-
-%% @doc curve25519_shared/2 creates a new shared secret from a given SecretKey and PublicKey.
-%% @end.
--spec curve25519_shared(SecretKey :: binary(), PublicKey :: binary()) -> binary().
-curve25519_shared(SecretKey, PublicKey) ->
-	enacl_nif:crypto_curve25519_scalarmult(SecretKey, PublicKey).
 
 %% Public Key Crypto
 %% ---------------------
@@ -623,6 +595,14 @@ onetime_auth_size() -> enacl_nif:crypto_onetimeauth_BYTES().
 %% @end
 -spec onetime_auth_key_size() -> pos_integer().
 onetime_auth_key_size() -> enacl_nif:crypto_onetimeauth_KEYBYTES().
+
+%% Curve 25519 Crypto
+%% ------------------
+%% @doc curve25519_scalarmult/2 does a scalar multiplication between the Secret and the BasePoint.
+%% @end.
+-spec curve25519_scalarmult(Secret :: binary(), BasePoint :: binary()) -> binary().
+curve25519_scalarmult(Secret, BasePoint) ->
+	enacl_nif:crypto_curve25519_scalarmult(Secret, BasePoint).
 
 %% Obtaining random bytes
 
--- src/enacl_ext.erl
+++ src/enacl_ext.erl
@@ -9,6 +9,13 @@
 	scramble_block_16/2
 ]).
 
+%% Curve25519
+-export([
+	curve25519_keypair/0,
+	curve25519_public_key/1,
+	curve25519_shared/2
+]).
+
 %% @doc scramble_block_16/2 scrambles (encrypt) a block under a given key
 %% The rules are that the block is 16 bytes and the key is 32 bytes. The block
 %% is scrambled by means of the (secret) key. This makes it impossible for an
@@ -23,3 +30,29 @@
 -spec scramble_block_16(binary(), binary()) -> binary().
 scramble_block_16(Block, Key) ->
     enacl_nif:scramble_block_16(Block, Key).
+
+%% Curve 25519 Crypto
+%% ------------------
+%% @doc curve25519_keypair/0 creates a new Public/Secret keypair.
+%%
+%% Generates and returns a new key pair for the Curve 25519 encryption scheme. The return value is a
+%% map in order to avoid using the public key as a secret key and vice versa.
+%% @end.
+-spec curve25519_keypair() -> #{ atom() => binary() }.
+curve25519_keypair() ->
+	<<B0:8/integer, B1:30/binary, B2:8/integer>> = enacl:randombytes(32),
+	SK = <<(B0 band 248), B1/binary, (64 bor (B2 band 127))>>,
+	PK = curve25519_public_key(SK),
+	#{ public => PK, secret => SK }.
+
+%% @doc curve25519_public_key/1 creates a public key from a given SecretKey.
+%% @end
+-spec curve25519_public_key(SecretKey :: binary()) -> binary().
+curve25519_public_key(SecretKey) ->
+	enacl:curve25519_scalarmult(SecretKey, <<9, 0:248>>).
+
+%% @doc curve25519_shared/2 creates a new shared secret from a given SecretKey and PublicKey.
+%% @end.
+-spec curve25519_shared(SecretKey :: binary(), PublicKey :: binary()) -> binary().
+curve25519_shared(SecretKey, PublicKey) ->
+	enacl:curve25519_scalarmult(SecretKey, PublicKey).
