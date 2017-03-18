2da2ba138f5807aec5bdd0c6a8f4706ec24aade6
Alexander Malaev <am@malaev.net>
2017-03-19 02:00:52 +0300

    Add kx_* functions from libsodium 1.0.12

--- c_src/enacl_nif.c
+++ c_src/enacl_nif.c
@@ -1028,6 +1028,101 @@ ERL_NIF_TERM enif_randombytes(ErlNifEnv *env, int argc, ERL_NIF_TERM const argv[
 	return enif_make_binary(env, &result);
 }
 
+/* Key exchange */
+
+static
+ERL_NIF_TERM enif_crypto_kx_SECRETKEYBYTES(ErlNifEnv *env, int argc, ERL_NIF_TERM const argv[]) {
+	return enif_make_int64(env, crypto_kx_SECRETKEYBYTES);
+}
+
+static
+ERL_NIF_TERM enif_crypto_kx_PUBLICKEYBYTES(ErlNifEnv *env, int argc, ERL_NIF_TERM const argv[]) {
+	return enif_make_int64(env, crypto_kx_PUBLICKEYBYTES);
+}
+
+static
+ERL_NIF_TERM enif_crypto_kx_SESSIONKEYBYTES(ErlNifEnv *env, int argc, ERL_NIF_TERM const argv[]) {
+	return enif_make_int64(env, crypto_kx_SESSIONKEYBYTES);
+}
+
+static
+ERL_NIF_TERM enif_crypto_kx_keypair(ErlNifEnv *env, int argc, ERL_NIF_TERM const argv[])
+{
+	ErlNifBinary pk, sk;
+
+	if (argc != 0) {
+		return enif_make_badarg(env);
+	}
+
+	if (!enif_alloc_binary(crypto_kx_PUBLICKEYBYTES, &pk)) {
+		return nacl_error_tuple(env, "alloc_failed");
+	}
+
+	if (!enif_alloc_binary(crypto_kx_SECRETKEYBYTES, &sk)) {
+		return nacl_error_tuple(env, "alloc_failed");
+	}
+
+	crypto_kx_keypair(pk.data, sk.data);
+
+	return enif_make_tuple2(env, enif_make_binary(env, &pk), enif_make_binary(env, &sk));
+}
+
+static
+ERL_NIF_TERM enif_crypto_kx_server_session_keys(ErlNifEnv *env, int argc, ERL_NIF_TERM const argv[])
+{
+	ErlNifBinary rx, tx, server_pk, server_sk, client_pk;
+
+	if ((argc != 3) ||
+			(!enif_inspect_binary(env, argv[0], &server_pk)) ||
+			(!enif_inspect_binary(env, argv[1], &server_sk)) ||
+			(!enif_inspect_binary(env, argv[2], &client_pk)) ||
+			(server_pk.size != crypto_kx_PUBLICKEYBYTES) ||
+			(server_sk.size != crypto_kx_SECRETKEYBYTES) ||
+			(client_pk.size != crypto_kx_PUBLICKEYBYTES)) {
+		return enif_make_badarg(env);
+	}
+
+	if (!enif_alloc_binary(crypto_kx_SESSIONKEYBYTES, &rx)) {
+		return nacl_error_tuple(env, "alloc_failed");
+	}
+
+	if (!enif_alloc_binary(crypto_kx_SESSIONKEYBYTES, &tx)) {
+		return nacl_error_tuple(env, "alloc_failed");
+	}
+
+	crypto_kx_server_session_keys(rx.data, tx.data, server_pk.data, server_sk.data, client_pk.data);
+
+	return enif_make_tuple2(env, enif_make_binary(env, &rx), enif_make_binary(env, &tx));
+}
+
+static
+ERL_NIF_TERM enif_crypto_kx_client_session_keys(ErlNifEnv *env, int argc, ERL_NIF_TERM const argv[])
+{
+	ErlNifBinary rx, tx, client_pk, client_sk, server_pk;
+
+	if ((argc != 3) ||
+			(!enif_inspect_binary(env, argv[0], &client_pk)) ||
+			(!enif_inspect_binary(env, argv[1], &client_sk)) ||
+			(!enif_inspect_binary(env, argv[2], &server_pk)) ||
+			(client_pk.size != crypto_kx_PUBLICKEYBYTES) ||
+			(client_sk.size != crypto_kx_SECRETKEYBYTES) ||
+			(server_pk.size != crypto_kx_PUBLICKEYBYTES)) {
+		return enif_make_badarg(env);
+	}
+
+	if (!enif_alloc_binary(crypto_kx_SESSIONKEYBYTES, &rx)) {
+		return nacl_error_tuple(env, "alloc_failed");
+	}
+
+	if (!enif_alloc_binary(crypto_kx_SESSIONKEYBYTES, &tx)) {
+		return nacl_error_tuple(env, "alloc_failed");
+	}
+
+	crypto_kx_client_session_keys(rx.data, tx.data, client_pk.data, client_sk.data, server_pk.data);
+
+	return enif_make_tuple2(env, enif_make_binary(env, &rx), enif_make_binary(env, &tx));
+}
+
 /* Various other helper functions */
 static
 void uint64_pack(unsigned char *y, ErlNifUInt64 x)
@@ -1194,6 +1289,13 @@ static ErlNifFunc nif_funcs[] = {
 	{"crypto_sign_ed25519_SECRETKEYBYTES", 0, enif_crypto_sign_ed25519_SECRETKEYBYTES},
 
 	{"randombytes", 1, enif_randombytes, ERL_NIF_DIRTY_JOB_CPU_BOUND},
+
+  {"crypto_kx_keypair", 0, enif_crypto_kx_keypair, ERL_NIF_DIRTY_JOB_CPU_BOUND},
+  {"crypto_kx_client_session_keys", 3, enif_crypto_kx_client_session_keys, ERL_NIF_DIRTY_JOB_CPU_BOUND},
+  {"crypto_kx_server_session_keys", 3, enif_crypto_kx_server_session_keys, ERL_NIF_DIRTY_JOB_CPU_BOUND},
+  {"crypto_kx_PUBLICKEYBYTES", 0, enif_crypto_kx_PUBLICKEYBYTES},
+  {"crypto_kx_SECRETKEYBYTES", 0, enif_crypto_kx_SECRETKEYBYTES},
+  {"crypto_kx_SESSIONKEYBYTES", 0, enif_crypto_kx_SESSIONKEYBYTES},
 
 	{"scramble_block_16", 2, enif_scramble_block_16}
 };
